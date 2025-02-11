06b6e3dd90c0f511d5a9752581ad120b9aa54954
LemyTruc <trucle.31221026452@st.ueh.edu.vn>
2025-02-11 10:30:28 +0700

    push

---
+++ Minimal APIs are architected to create HTTP APIs with minimal dependencies/TodoApi/TodoApi/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string IsComplete { get; set; }
+
+    }
+}
